5e0e3edc880a4b7127b68da178f2d07d556d16e7
Raphael <paiva.raphaelt@gmail.com>
2021-12-20 00:42:46 +0100

    refacto: add nullable option

--- src/oldPassword/oldPassword.entity.ts
+++ src/oldPassword/oldPassword.entity.ts
@@ -7,9 +7,9 @@ export class OldPassword extends DateEntity {
   @PrimaryGeneratedColumn('uuid')
   public idOldPassword: string;
 
-  @Column()
+  @Column({ type: 'varchar', length: 20, nullable: false })
   public password: string;
 
-  @ManyToOne(() => Account, (account) => account.idAccount)
+  @ManyToOne(() => Account, (account) => account.idAccount, { nullable: false })
   public account: Account;
 }

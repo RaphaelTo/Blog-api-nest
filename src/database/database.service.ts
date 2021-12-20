4cfd9c6ee022ce5d89fd7d0b37759a94dd316370
Raphael <paiva.raphaelt@gmail.com>
2021-12-20 01:08:30 +0100

    add: all Entity

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -1,5 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import { Account } from '../account/account.entity';
+import { Article } from '../article/article.entity';
+import { Category } from '../category/category.entity';
+import { Comment } from '../comment/comment.entity';
+import { CommentRecursive } from '../commentRecursive/commentRecursive.entity';
+import { OldPassword } from '../oldPassword/oldPassword.entity';
+import { Rank } from '../rank/rank.entity';
+import { ResetPassword } from '../resetPassword/resetPassword.entity';
+import { RulesAccount } from '../rulesAccount/rulesAccount.entity';
+import { Tag } from '../tag/tag.entity';
+import { User } from '../user/User.entity';
 
 @Injectable()
 export class DatabaseService implements TypeOrmOptionsFactory {
@@ -11,7 +22,19 @@ export class DatabaseService implements TypeOrmOptionsFactory {
       username: process.env.DATABASE_USERNAME,
       password: process.env.DATABASE_PASSWORD,
       database: process.env.DATABASE_NAME,
-      entities: [],
+      entities: [
+        Account,
+        Article,
+        Category,
+        Comment,
+        CommentRecursive,
+        OldPassword,
+        Rank,
+        ResetPassword,
+        RulesAccount,
+        Tag,
+        User,
+      ],
       synchronize: true,
     };
   }

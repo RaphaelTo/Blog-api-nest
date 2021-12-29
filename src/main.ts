fc71d7eef1b72dce28919c822fafa1abcf0076ab
Raphael <paiva.raphaelt@gmail.com>
2021-12-29 18:45:34 +0100

    add config for swagger api

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Blog api')
+    .setDescription("Il s'agit de l'api blog")
+    .build();
+  const doc = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/apidoc', app, doc);
   await app.listen(3000);
 }
 bootstrap();

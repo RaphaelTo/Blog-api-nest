3944cb3731a46f7c5789fdd5eb9e88987e359c4c
Raphael <paiva.raphaelt@gmail.com>
2021-12-29 18:47:27 +0100

    add AccountModule and rulesAccountModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,16 @@ import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 
 import { UserModule } from './user/user.module';
+import { AccountModule } from './account/account.module';
+import { RulesAccountModule } from './rulesAccount/rulesAccount.module';
 
 @Module({
-  imports: [DatabaseModule, ConfigModule.forRoot(), UserModule],
+  imports: [
+    DatabaseModule,
+    ConfigModule.forRoot(),
+    UserModule,
+    AccountModule,
+    RulesAccountModule,
+  ],
 })
 export class AppModule {}
